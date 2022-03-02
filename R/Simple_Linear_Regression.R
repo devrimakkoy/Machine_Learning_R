13e2ca993080ba6b8d9a6c87dbbe5a50d2d407d4
Devrim Akköy <99104978+devrimakkoy@users.noreply.github.com>
2022-03-02 12:31:27 +0300

    initial commit

--- R/Simple_Linear_Regression.R
+++ R/Simple_Linear_Regression.R
@@ -32,7 +32,7 @@ ggplot() +
   xlab('Years of experience') +
   ylab('Salary')
 
-# Visualising the Test set results
+#Visualising the Test set results
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
